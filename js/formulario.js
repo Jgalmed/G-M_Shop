c899d1947514b7e139a92ea70d180bc8343cf601
Jorge <jgallegos.jg99@gmail.com>
2023-04-30 00:51:24 -0500

    nuevos cambios

--- js/formulario.js
+++ js/formulario.js
@@ -3,9 +3,11 @@ const productoscarrito = localStorage.getItem('productos-en-carrito');
 const botonEnviar = document.querySelector('#boton-form');
 const produtosComprados = JSON.parse(productoscarrito); 
 const productosEnviar = document.querySelector('#productos-enviar');
-let enviar = [];
-enviar = produtosComprados.map(function (elem) {
+let enviar = produtosComprados.map(function(elem) {
     let returnObjeto = { id: elem.id, cantidad: elem.cantidad, precio: elem.precio};
       return returnObjeto;
   });
-productosEnviar.innerHTML = JSON.stringify(enviar);
+  let productosSend = JSON.stringify(enviar);
+  console.log(productosSend);
+   productosEnviar.innerHTML = productosSend;
+  
